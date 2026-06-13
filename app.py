13d87b6274cd0e0518977ed24534ba7c93367363
Rodrigo Filho <r.sotero2008@gmail.com>
2026-06-13 16:05:17 -0300

    Feat: Conclusão do quadrobase.html, extenção nas páginas quadro, gastos, renda, membros e config e esboço das páginas index, cadastro e login

--- app.py
+++ app.py
@@ -17,30 +17,30 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/quadros/')
+@app.route('/quadros', methods=['GET', 'POST'])
 def quadros():
     return render_template('quadros.html')
 
 @app.route('/quadros/quadro')
 def quadro():
-    return render_template('quadro.html')
+    return render_template('quadro.html', secao=1)
 
 
 @app.route('/quadros/quadro/gastos')
 def gastos():
-    return render_template('gastos.html')
+    return render_template('gastos.html', secao=2)
 
 
 @app.route('/quadros/quadro/renda')
 def renda():
-    return render_template('renda.html')
+    return render_template('renda.html', secao=3)
 
 
 @app.route('/quadros/quadro/membros')
 def membros():
-    return render_template('membros.html')
+    return render_template('membros.html', secao=4)
 
 
-@app.route('/quadros/quadro/configuracao')
+@app.route('/quadros/quadro/configuracoes')
 def config():
-    return render_template('config.html')
+    return render_template('config.html', secao=5)
